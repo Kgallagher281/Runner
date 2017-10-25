134ac6e0035fba726688534fd5e754d7003be97f
christykusuma <christykusuma96@gmail.com>
2017-10-25 12:22:13 -0400

    done with scaffolding

---
+++ app/views/distances/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @distances, partial: 'distances/distance', as: :distance
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :distances
+  resources :posts
+  resources :races
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
