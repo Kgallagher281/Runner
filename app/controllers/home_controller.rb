179f2770cab92b9f2ad292942444ca4a86bc5c1d
christykusuma <christykusuma96@gmail.com>
2017-10-26 10:42:38 -0400

    login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,17 @@
 class HomeController < ApplicationController
+
+	def index
+
+	end
+
+  # Post method to handle login
+  def handle_login
+    user = User.find_by(username: params[:user][:username])
+    if user && user.password == params[:user][:password]
+      session[:user_id]= user.id
+      redirect_to posts_path
+    else
+      redirect_to :root
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,18 @@ Rails.application.routes.draw do
   resources :races
   resources :users
 
-  get '/' => "home#index"
+  # Go straight to home index page
+  root 'home#index'
+
+  # If login form is filled, handle login
+  post 'login' => "home#handle_login", as: :login
+
+  # If new user, lead to signup form
+  get 'signup' => "home#signup", as: :signup
+
+  # If signup form is filled, handle signup
+  post 'signup' => "home#handle_signup"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
